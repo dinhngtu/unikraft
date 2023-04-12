8d822fbe37ef3eb9eb71bf52b43fa3d09fde0370
Tu Dinh Ngoc <dinhngoc.tu@irit.fr>
2023-04-12 15:38:55 +0000

    configurable uk_bootinfo size up to 128 memregions

--- plat/common/bootinfo.c
+++ plat/common/bootinfo.c
@@ -11,7 +11,7 @@
  * Space reservation for boot information. Will be populated by build script.
  */
 __section(".uk_bootinfo") __align(8) __used
-static char bi_bootinfo_sec[UKPLAT_BOOTINFO_SIZE(32)];
+static char bi_bootinfo_sec[UKPLAT_BOOTINFO_SIZE(CONFIG_UKPLAT_BOOTINFO_NREGIONS)];
 
 /**
  * Pointer to boot information. Can be changed at runtime if the boot
